5ce2e8fa58d952ddcadf430e991fd0bf8dc16cc6
Tóth Gergő <tgery3@gmail.com>
2020-09-23 18:56:47 +0200

    Initial Commit

---
+++ mosze3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  mosze3
+//
+//  Created by Tóth Gergő on 2020. 09. 23..
+//  Copyright © 2020. NullPointerExceptionTeam. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
