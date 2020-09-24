c77f338c82fd611ed984e4587f21efccfd4fa3ce
Tóth Gergő <tgery3@gmail.com>
2020-09-24 16:38:31 +0200

    The script now running all the cases

--- run_test.sh
+++ run_test.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-rm units/output.txt
+rm output.txt
 touch output.txt
 
-./a.out units/hulk.json units/capt.json >> units/output.txt
+./a.out units/capt.json units/hulk.json >> output.txt
+./a.out units/capt.json units/ironman.json >> output.txt
+./a.out units/hulk.json units/capt.json >> output.txt
+./a.out units/hulk.json units/ironman.json >> output.txt
+./a.out units/ironman.json units/hulk.json >> output.txt
+./a.out units/ironman.json units/capt.json >> output.txt
