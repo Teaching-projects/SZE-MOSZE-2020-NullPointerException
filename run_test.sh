0f23e252b9a6c9912795c4ecd38a013c6346fb91
Tóth Gergő <tgery3@gmail.com>
2020-09-24 17:21:42 +0200

    The script is now checking the diff between outputs

--- run_test.sh
+++ run_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rm output.txt
+rm -f output.txt
 touch output.txt
 
 ./a.out units/capt.json units/hulk.json >> output.txt
@@ -9,3 +9,12 @@ touch output.txt
 ./a.out units/hulk.json units/ironman.json >> output.txt
 ./a.out units/ironman.json units/hulk.json >> output.txt
 ./a.out units/ironman.json units/capt.json >> output.txt
+
+diff output.txt units/results.txt
+
+if [ $? -eq 0 ]
+then
+echo "No differences. The test was OK!"
+else
+echo "Test failed."
+fi
