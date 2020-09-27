a96846155659799ab185bd656f114e891c189ad6
Tóth Gergő <tgery3@gmail.com>
2020-09-27 16:23:14 +0200

    Reworking main, deleted default inits, exception handling

--- main.cpp
+++ main.cpp
@@ -2,45 +2,32 @@
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    //Hős1 és Hős2 létrehozása
-    Hero *warrior1;
-    Hero *warrior2;
-    
     try {
         if(argc == 3){
+            Hero *warrior1 = new Hero(Hero::parseUnit(argv[1]));
+            Hero *warrior2 = new Hero(Hero::parseUnit(argv[2]));
             
-            warrior1 = new Hero(Hero::parseUnit(argv[1]));
-            warrior2 = new Hero(Hero::parseUnit(argv[2]));
+            while(!warrior2->isDead() && !warrior1->isDead()){
+                warrior1->damaging(warrior2);
+                warrior2->damaging(warrior1);
+            }
+            if(warrior1->isDead()){
+                std::cout << warrior2->getName() << " wins. Remaining HP: " << warrior2->getHP() << std::endl;
+            }else{
+                std::cout << warrior1->getName() << " wins. Remaining HP: " << warrior1->getHP() << std::endl;
+            }
+            
+            delete warrior1;
+            delete warrior2;
+            return 0;
         }else{
-            std::cout << "Bad arg inputs. Initializing default parameters." << std::endl;
-            warrior1 = new Hero("Superman", 150, 20);
-            warrior2 = new Hero("Batman", 200, 30);
+            std::cout << "Bad arg inputs." << std::endl;
+            return 1;
         }
     } catch (std::exception& e) {
-        std::cout << "Bad inputs or file not exists. Initializing default parameters."
+        std::cout << "Exception: " << e.what()
         << std::endl;
-        warrior1 = new Hero("Superman", 150, 20);
-        warrior2 = new Hero("Batman", 200, 30);
+        return 2;
     }
-    
-    
-    //A játék lefolyása
-    while(!warrior2->isDead() && !warrior1->isDead()){
-        warrior1->damaging(warrior2);
-        warrior2->damaging(warrior1);
-    }
-    
-    
-    if(warrior1->isDead()){
-        std::cout << warrior2->getName() << " wins. Remaining HP: " << warrior2->getHP() << std::endl;
-    }else{
-        std::cout << warrior1->getName() << " wins. Remaining HP: " << warrior1->getHP() << std::endl;
-    }
-    
-    
-    delete warrior1;
-    delete warrior2;
-    
-    return 0;
     
 }
