6fee95bbd32c61858f2e0e02d706cd2ff9d104cb
Tóth Gergő <tgery3@gmail.com>
2020-10-14 21:28:20 +0200

    Added test unit and testing the gtest tester

--- test/unit_test.cpp
+++ test/unit_test.cpp
@@ -2,6 +2,21 @@
 #include <map>
 #include <fstream>
 #include <gtest/gtest.h>
+#include "../JsonParser.h"
+
+TEST(ParserTest, FilenameInputTest){
+    std::map<std::string, std::string> expected;
+    std::map<std::string, std::string> jsontest;
+    
+    expected.insert(std::pair<std::string, std::string>("name", "Monster"));
+    expected.insert(std::pair<std::string, std::string>("hp", "10000"));
+    expected.insert(std::pair<std::string, std::string>("dmg", "100"));
+    
+    jsontest = JsonParser::parser("test/test_warrior.json");
+    
+    ASSERT_EQ(expected, jsontest);
+    
+}
 
 
 int main(int argc, char** argv)
