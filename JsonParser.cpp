7e28bc2e197cb7b6ef2d90c216888f0261ed2d46
Tóth Gergő <tgery3@gmail.com>
2020-10-14 12:55:26 +0200

    Modified JsonParser for test

--- JsonParser.cpp
+++ JsonParser.cpp
@@ -1,2 +1,36 @@
-#include <stdio.h>
 #include "JsonParser.h"
+
+std::map<std::string, std::string> JsonParser::parseFile(std::istream& istream){
+    std::string Data;
+    if(istream.good()){
+        std::string line = "";
+        while(std::getline(istream, line)){
+            Data += line;
+        }
+    }
+    
+    return JsonParser::StringFinder(Data);
+}
+
+std::map<std::string, std::string> JsonParser::StringFinder(std::string& Data){
+    std::map<std::string, std::string> Map;
+    std::string readline = "";
+    
+    for (int i = 0; i < Data.size(); i++) {
+        if (isalnum(Data[i])) {
+            if (readline == "name"){
+                Map["name"] += Data[i];
+            }
+            else if (readline == "hp"){
+                Map["hp"] += Data[i];
+            }
+            else if (readline == "dmg"){
+                Map["dmg"] += Data[i];
+            }
+            else readline += Data[i];
+        }
+    }
+    readline = "";
+    
+    return Map;
+}
