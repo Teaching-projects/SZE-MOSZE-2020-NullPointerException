a7c8eb73ffdf87a40ade6d5e73d957c9a9303417
hegrobaa <43639808+hegrobaa@users.noreply.github.com>
2020-10-15 14:25:25 +0200

    Added lvlup engine

--- AdvancedHero.cpp
+++ AdvancedHero.cpp
@@ -7,3 +7,25 @@ int AdvancedHero::xpHatar = 100;
 AdvancedHero::AdvancedHero(const std::string& name, int hp, const int dmg) : Hero(name, hp, dmg), xp(0), maxHp(hp){
     
 }
+
+AdvancedHero AdvancedHero::parseUnit(const std::string& filename){
+    Hero Unit = Hero::parseUnit(filename);
+    return AdvancedHero(Unit.getName(), Unit.getHP(), Unit.getDmg());
+}
+
+void AdvancedHero::advancedDamage(AdvancedHero* enemy){
+    xp += std::min(dmg, enemy->getHP());
+    this->damaging(enemy);
+    levelup(this->xp / xpHatar);
+}
+
+void AdvancedHero::levelup(int levelcount){
+    for(int i=0; i<levelcount; i++){
+        maxHp = (int)round((double)maxHp * 1.1);
+        dmg = (int)round((double)dmg * 1.1);
+        hp = maxHp;
+        lvl++;
+        xp -= xpHatar;
+    }
+    
+}
