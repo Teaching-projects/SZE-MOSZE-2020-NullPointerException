4ed3e25998e981b75b080a055c3e79462367c292
VarietyF <martinz.kovacs@gmail.com>
2020-10-15 14:42:54 +0200

    Hero.cpp updated with new battle method

--- Hero.cpp
+++ Hero.cpp
@@ -46,17 +46,35 @@ void Hero::Battle(Hero* target){
     double tempw2 = 0;
 
     this->damaging(target);
+
+    while(!this->isDead() && !target->isDead()){
+        if(w1+tempw1 < w2+tempw2) {
+            tempw1 += w1;
+            this->damaging(target);
+        }else if(w1+tempw1 > w2+tempw2){
+            tempw2 += w2;
+            target->damaging(this);
+        }else{
+            tempw1 += w1;
+            this->damaging(target);
+        }
+    }
+}
+
+double Hero::getAttackSpeed() const{
+    return attackspeed;
 }
 
 Hero Hero::parseUnit(const std::string& fileName){
     std::fstream file;
     file.open(fileName);
-    std::string readline, name, hp, dmg, line;
+    std::string readline, name, hp, dmg, attackspeed, line;
+    
     
     if(file.is_open()){
         while (getline(file, line)) {
             for (int i = 0; i < line.size(); i++) {
-                if (isalnum(line[i])) {
+                if (isalnum(line[i]) or line[i] == '.') {
                     if (readline == "name"){
                         name += line[i];
                     }
@@ -66,19 +84,19 @@ Hero Hero::parseUnit(const std::string& fileName){
                     else if (readline == "dmg"){
                         dmg += line[i];
                     }
+                    else if (readline == "attackcooldown"){
+                        attackspeed += line[i];
+                    }
                     else readline += line[i];
                 }
             }
             readline = "";
         }
         file.close();
-        return Hero(name,stoi(hp),stoi(dmg));
+        return Hero(name,stoi(hp),stoi(dmg),stod(attackspeed));
     }else{
         throw std::runtime_error(fileName + " not exist.");
     }
-    
-    
-    
 }
 
 
