838cc17ff0ab6d0db755543c749245a6d1c827e0
VarietyF <martinz.kovacs@gmail.com>
2020-11-01 11:49:18 +0100

    updated jsonparser header comments

--- JsonParser.h
+++ JsonParser.h
@@ -20,11 +20,36 @@
 //This is the class of the JsonParser
 class JsonParser{
 private:
-    static std::string WhitespaceCleanerAndFormatChecker(std::string&);  ///< This method is used to check if the json file is in the correct format. Also it cleanes out the blank spaces.
+    
+    /**
+     * \brief Json file format checking, and cleaning out the blank spaces
+     *
+     * \return Returns a string that has the json files data
+     */
+    static std::string WhitespaceCleanerAndFormatChecker(std::string&);
+
 public:
-    const static std::map<std::string, std::string> StringFinder(const std::string&);  ///<  This method is used to find the hearo's attributes. It looks for the hero's name, health, damage and attackcooldown and maps them together.
-    static std::map<std::string, std::string> parser(const std::string&);  ///< This method is responsible to open the json files.
-    static std::map<std::string, std::string> parseFile(std::istream&);  ///<  This method is used to read in the json files data.
+
+    /**
+     * \brief This method is used to find the hearo's attributes. It looks for the hero's name, health, damage and attackcooldown and maps them together.
+     *
+     * \return Returns the correct Map file of a hero if everythings good. If there's a problem we get a "Bad mapping" warning.
+     */
+    const static std::map<std::string, std::string> StringFinder(const std::string&);
+    
+    /**
+     * \brief This method is responsible to open the json files.
+     *
+     * \return Returns a Map if the json file exist. If there's a problem, we get a "not exist" warning.
+     */
+    static std::map<std::string, std::string> parser(const std::string&);
+    
+    /**
+     * \brief This method is used to read in the json files data.
+     *
+     * \return Returns with the data of the json file.
+     */
+    static std::map<std::string, std::string> parseFile(std::istream&);
 };
 
 #endif /* JsonParser_h */
