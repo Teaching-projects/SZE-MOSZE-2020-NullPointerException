ec680931d217043e1ef097b545ff0407571ba8ff
VarietyF <martinz.kovacs@gmail.com>
2020-11-01 10:38:29 +0000

    Update gh-pages to output generated at a01ef2b

--- search/all_8.js
+++ search/all_8.js
@@ -1,4 +1,6 @@
 var searchData=
 [
+  ['parsefile',['parseFile',['../classJsonParser.html#aa16485fdf6c1d6b31ab91e873ec5d54d',1,'JsonParser']]],
+  ['parser',['parser',['../classJsonParser.html#a1a57ae294c3cf6e09f70936416f9b84a',1,'JsonParser']]],
   ['parseunit',['parseUnit',['../classAdvancedHero.html#a0cec89be05b833da1b67cd659d4dfcb9',1,'AdvancedHero::parseUnit()'],['../classHero.html#a927e9b998701d26e7bfe439f1395926c',1,'Hero::parseUnit()']]]
 ];
--- search/all_9.js
+++ search/all_9.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['sze_2dmosze_2d2020_2dnullpointerexception',['SZE-MOSZE-2020-NullPointerException',['../autotoc_md0.html',1,'']]],
-  ['status',['status',['../classAdvancedHero.html#ae8c7cc01d7d6ae2ca58ff1af2ad6f66a',1,'AdvancedHero']]]
+  ['status',['status',['../classAdvancedHero.html#ae8c7cc01d7d6ae2ca58ff1af2ad6f66a',1,'AdvancedHero']]],
+  ['stringfinder',['StringFinder',['../classJsonParser.html#a2de060e911682fb27fe89d69ecae980c',1,'JsonParser']]]
 ];
