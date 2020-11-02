11a4b7bbb7506ebf5acfc4a3108be905b78db2dd
VarietyF <martinz.kovacs@gmail.com>
2020-11-02 19:25:26 +0100

    added requested changes

--- JsonParser.h
+++ JsonParser.h
@@ -22,25 +22,27 @@ class JsonParser{
 private:
     
     /**
-     * \brief Json file format checking, and cleaning out the blank spaces
-     *
-     * \return Returns a string that has the json files data
+     * \brief After the json files data goes through the stringfinder method, the given string is then analized in this method. This method deletes the unnecessary spaces from the beginning. Also it looks if the string has any value.
+     * 
+     * \param This gets a string from the stringfinder method
+     * 
+     * \return Returns a string that has the json files data, without blank spaces
      */
-    static std::string WhitespaceCleanerAndFormatChecker(std::string&);
+    static std::string WhitespaceCleanerAndFormatChecker(std::string& inputValue);
 
 public:
 
     /**
-     * \brief This method is used to find the hearo's attributes. It looks for the hero's name, health, damage and attackcooldown and maps them together.
+     * \brief This method is used to find the hero's attributes. It looks for the hero's name, health, damage and attackcooldown and maps them together.
      *
-     * \return Returns the correct Map file of a hero if everythings good. If there's a problem we get a "Bad mapping" warning.
+     * \return Returns the correct Map file of a hero if everythings good. If there's a problem we get a "Bad mapping" runtime error.
      */
     const static std::map<std::string, std::string> StringFinder(const std::string&);
     
     /**
      * \brief This method is responsible to open the json files.
      *
-     * \return Returns a Map if the json file exist. If there's a problem, we get a "not exist" warning.
+     * \return Returns a Map if the json file exist. If there's a problem, we get a "not exist" runtime error.
      */
     static std::map<std::string, std::string> parser(const std::string&);
     
