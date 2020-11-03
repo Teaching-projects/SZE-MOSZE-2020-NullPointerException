9654b9f2e272b37e3b338b47c3abbead425f87fa
VarietyF <martinz.kovacs@gmail.com>
2020-11-03 19:17:34 +0000

    Update gh-pages to output generated at 05d299e

--- search/all_3.js
+++ search/all_3.js
@@ -3,7 +3,6 @@ var searchData=
   ['getattackcooldown',['getAttackCooldown',['../classHero.html#ac10c378c0951f28ffefd4d54c08a4f2e',1,'Hero']]],
   ['getdmg',['getDmg',['../classHero.html#a60e7b5be59457128423fee62b254721f',1,'Hero']]],
   ['gethp',['getHP',['../classHero.html#acb8f0cd73372759f221aaa30b65c098d',1,'Hero']]],
-  ['getlvl',['getLvl',['../classAdvancedHero.html#a528a1d293424a2e6ca483aede08a2ea4',1,'AdvancedHero']]],
   ['getname',['getName',['../classHero.html#a972b20eae12fd58c09b6e84deb434b57',1,'Hero']]],
   ['getxp',['getxp',['../classAdvancedHero.html#a255247f6fdb7bbdbb80bce28010422da',1,'AdvancedHero']]]
 ];
