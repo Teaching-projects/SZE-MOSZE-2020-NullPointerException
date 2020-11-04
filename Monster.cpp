625b1329a6257fc3ff2e39e31f958f0249583182
Tóth Gergő <tgery3@gmail.com>
2020-11-04 16:43:58 +0100

    missing const fixed again

--- Monster.cpp
+++ Monster.cpp
@@ -3,7 +3,7 @@
 
 Monster::Monster(const std::string& name, int hp, int dmg, double as) : Character(name, hp,  dmg, as) {}
 
-Monster Monster::parse(std::string& filename){
+Monster Monster::parse(const std::string& filename){
     JSON MonsterMap = JSON::parseFromFile(filename);
     
     return Monster(MonsterMap.get<std::string>("name"), MonsterMap.get<int>("health_points"), MonsterMap.get<int>("damage"), MonsterMap.get<int>("attack_cooldown"));
